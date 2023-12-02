4aca6d947224dd24ac33f7a96b2419161a18a2e0
bityutskiyna <bityutskiyna@gmail.com>
2023-12-02 13:47:55 +0300

    Added model product

--- product_catalog/settings.py
+++ product_catalog/settings.py
@@ -1,22 +1,12 @@
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-rjjxxl(6q+fh3zi@e6q9b@ylpo=6#co+t-h4yh5##pxp-qkc9r'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -59,19 +49,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'product_catalog.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -88,10 +75,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -100,13 +83,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- products/models.py
+++ products/models.py
@@ -1,5 +1,31 @@
 from django.db import models
 
 
-class Product(models.Model):
-    name = models.CharField(max_length=1000)
+class DateTimeMixin(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Images(DateTimeMixin):
+    link = models.CharField(max_length=1000)
+
+
+class Product(DateTimeMixin):
+    type = [
+        ("foods", "foods"),
+        ("dishes", "dishes"),
+        ("clothing", "clothing"),
+    ]
+    name = models.CharField(max_length=500)
+    article = models.CharField(max_length=10, default='')
+    description = models.CharField(max_length=5000)
+    image = models.ForeignKey(Images, on_delete=models.CASCADE)
+    product_type = models.CharField(max_length=50, choices=type)
+    price = models.IntegerField(default=0)
+    quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
